424cc30df01422208b2bb23c3598033c2d015e26
aShnarkevich <petrikov05@gmail.com>
2020-12-02 13:31:54 +0300

    update test

--- web-module/src/test/java/com/gmail/petrikov05/app/web/controller/security/LoginControllerSecurityTest.java
+++ web-module/src/test/java/com/gmail/petrikov05/app/web/controller/security/LoginControllerSecurityTest.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -14,6 +15,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
+@TestPropertySource("/application-integration.properties")
 class LoginControllerSecurityTest {
 
     @Autowired
